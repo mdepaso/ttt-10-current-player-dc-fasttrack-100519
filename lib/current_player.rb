9461cfa4aaf065cafa97604c9599e4d527c1eeb0
Matthew DePaso <matthewhdepaso@gmail.com>
2019-10-06 15:53:40 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,7 +4,8 @@ counter=0
 
 #needs to count the number of X and Os on the board.
 
-board.each do
+board.each do |count|
+  
 
 while counter <= board.length-1
   puts "It is turn #{board[counter]}!"
