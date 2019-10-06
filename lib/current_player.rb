5951dc60acad3dd5852020a609eeb20be441e873
Matthew DePaso <matthewhdepaso@gmail.com>
2019-10-06 16:12:01 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -13,47 +13,5 @@ end
 #   counter += 1
 # end
 
-board = [" ","X","O"," "," "," "," "," "," ",]
-turn_count(board
-
-
-
-
-
-
-
-
-
-
-
-
-
-def turn_count(board)
-
-counter=0
-
-#needs to count the number of X and Os on the board.
-# just return a number of turns. How many elements inside the array are an X or an O
-
-board.each do |count|
-  # If == X or O add one. 
-
-while counter <= board.length-1
-  # puts "It is turn #{board[counter]}!"
-  counter = counter + 1
-  end
-
-end
-
-
-def current_player(board)
-  # turn_count check if it's an X or an O
-  # Check if it's an even number
-  # If even return X. And say it's player X
-  # Else return O
-  
-  turn-count % 2 #=> 0 
-  
-
-  
-end
+board = [" ","X","O"," "," "," "," "," "," "]
+turn_count(board)
