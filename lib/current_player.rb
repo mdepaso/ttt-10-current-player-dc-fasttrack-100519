7a575ad7e03a7c2593064a5c6c94f6cb62c56a31
Matthew DePaso <matthewhdepaso@gmail.com>
2019-10-06 16:43:05 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,24 @@
+def turn_count(board)
+  counter = 0
+  board.each do |space|
+    if space == "O" || space == "X"
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  
+  if turn_count(board).even?
+    puts "X"
+    return "X"
+  else turn_count(board).odd?
+    puts "O"
+    return "O"
+  end
+  
+end
+  
+board=["X","O","X"]
+current_player(board) 
