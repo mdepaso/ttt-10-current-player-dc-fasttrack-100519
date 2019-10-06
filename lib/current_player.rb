4a492566a017ce220713e30a0cd28077ef26bfd4
Matthew DePaso <matthewhdepaso@gmail.com>
2019-10-06 15:27:41 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+
+
+def turn_count
+
+end
+
+
+def current_player(board)
+  # turn_count check if it's an X or an O
+  # Check if it's an even number
+  # If even return X. And say it's player X
+  # Else return O
+  
+  turn-count % 2 #=> 0 
+  
+
+  
+end
